a75344fa4bd7d89f5e3b29f9d9bb9582093cd503
Marguerite Butler <mbutler808@gmail.com>
2023-03-13 12:42:15 -1000

    100 reps of merge and nomerge

--- model_search/output_nomerge_10reps/iqtreerun.sh
+++ model_search/output_nomerge_10reps/iqtreerun.sh
@@ -1,2 +1,5 @@
 #for i in `seq 1 100`; do cp  asterophryinae_15partitions.nex nomerge$(printf "%02d" $i).nex; done
-for i in `seq 1 10`; do ../../iqtree-2.2.0-Windows/bin/iqtree2 -s asterophryinae_20230305.phy -p nomerge$(printf "%02d" $i).nex -m TEST -B 1000 -o "UMMZ219489_Scaphiophryne_marmorata" --date asterophryinae_dates.txt --date-tip 0 -mset JC,F81,K80,HKY,TN,TNe,TPM2,TPM2u,TPM3,TPM3u,TIM,TIMe,TIM2,TIM2e,TIM3,TIM3e,TVMe,TVM,SYM,GTR ; done
+for i in `seq 11 100`; 
+do 
+  ../../../../iqtree-2.2.0-Windows/bin/iqtree2 -s asterophryinae_20230305.phy -p nomerge$(printf "%02d" $i).nex -m TESTONLY -o "UMMZ219489_Scaphiophryne_marmorata" --date asterophryinae_dates.txt --date-tip 0 -mset JC,F81,K80,HKY,TN,TNe,TPM2,TPM2u,TPM3,TPM3u,TIM,TIMe,TIM2,TIM2e,TIM3,TIM3e,TVMe,TVM,SYM,GTR ; 
+done
