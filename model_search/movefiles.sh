1f5e0e9431b8cfb0079c6c231ae882a5c55f5da9
Marguerite Butler <mbutler808@gmail.com>
2023-03-22 10:44:58 -1000

    split output folders into smaller directories

---
+++ model_search/movefiles.sh
@@ -0,0 +1,6 @@
+mkdir output_100reps_extra
+mv output_10reps/*.gz output_100reps_extra/
+mv output_10reps/*.nwk output_100reps_extra/
+mkdir output_nomerge_100reps_extra
+mv output_nomerge_10reps/*.gz output_nomerge_100reps_extra/
+mv output_nomerge_10reps/*.nwk output_nomerge_100reps_extra/
