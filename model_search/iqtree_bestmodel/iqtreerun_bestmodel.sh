230f667208b4e599e85b1cc502d3054290326780
Marguerite Butler <mbutler@hawaii.edu>
2023-03-23 13:35:19 -1000

    drop model set from iqtree command

--- model_search/iqtree_bestmodel/iqtreerun_bestmodel.sh
+++ model_search/iqtree_bestmodel/iqtreerun_bestmodel.sh
@@ -1,2 +1,2 @@
 # Best model is nomerge08
-../../../../iqtree-2.2.0-Windows/bin/iqtree2 -s asterophryinae_20230305.phy -p nomerge08.nex.best_scheme.nex -B 1000 -o "UMMZ219489_Scaphiophryne_marmorata" --date asterophryinae_dates.txt --date-tip 0 -mset JC,F81,K80,HKY,TN,TNe,TPM2,TPM2u,TPM3,TPM3u,TIM,TIMe,TIM2,TIM2e,TIM3,TIM3e,TVMe,TVM,SYM,GTR ; 
+../../../../iqtree-2.2.0-Windows/bin/iqtree2 -s asterophryinae_20230305.phy -p nomerge08.nex.best_scheme.nex -B 1000 -o "UMMZ219489_Scaphiophryne_marmorata" --date asterophryinae_dates.txt --date-tip 0 ; 
