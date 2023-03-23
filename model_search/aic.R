5682e7d69c16dc0ed3ab14a715fe2d7d84a0ffe3
Marguerite Butler Anolis Pro <mbutler808@gmail.com>
2023-03-22 18:58:32 -1000

    added comments and updated aic.R

--- model_search/aic.R
+++ model_search/aic.R
@@ -1,31 +1,36 @@
-dir <- "output_10reps"
-outfile <- "trees_from_partitionfinder_models_aic.txt"
+dir <- "output_100reps"
+outfile <- "best_model_partitionfinder_aic.txt"
 
 summaryaic <- function( dir, outfile) {
-	files <- list.files(path=dir)
-	files <- grep("nex.iqtree", files, value=T)
-	files <- paste(dir, files, sep="/")
+	files <- list.files(path=dir)                          # get list of filenames from the directory
+	files <- grep("nex.iqtree", files, value=T)  # get files ending in nex.iqtree
+	files <- paste(dir, files, sep="/")                # paste the path to the filename
 	
-	lines <- sapply( files, function(x) {
-					        d <- readLines(x)
-					        d[grepl("^Akaike", d)] 
-					      }  
+	lines <- sapply( files, function(x) {            # for each file 		
+					        d <- readLines(x)             # read the lines 
+					        d[grepl("^Akaike", d)]       # select lines that start with Akaike 
+					      }                                         # lines is a vector lines that start with Akaike
 				       ) 
 				       
-	vals <- gsub(".*?([0-9]+.[0-9]+).*", "\\1", lines)
-	runs <- names(vals)
-	aic <- as.numeric(vals)
+	vals <- gsub(".*?([0-9]+.[0-9]+).*", "\\1", lines)    # for each line, extract the decimal number. 
+	runs <- names(vals)                                           # the names hold the run number
+	aic <- as.numeric(vals)                                      # coerce to numeric, save as aic
 	
-	dat <- data.frame( runs=runs, aic=aic)
-	dat <- dat[ order(dat$aic), ]
-	
-	sink(outfile)
-	
-	cat("AIC scores of trees from 10 runs after PartitionFinder and tree estimation:\n")
-	print(dat)
-	cat(paste("delta aic worst - best: ", round(max(dat$aic) - min(dat$aic))))
-	sink()
+	dat <- data.frame( runs=runs, aic=aic)     # make a dataframe from run name and aic score
+	dat <- dat[ order(dat$aic), ]                      # sort by aic score
+
+    return(dat)
 }
 
-summaryaic(dir="output_10reps", outfile="AIC_partifionfinder_10runs.txt" )
-summaryaic(dir="output_nomerge_10reps", outfile="AIC_modelselection_nomerge_10runs.txt" )
+
+aic1 <- summaryaic(dir="output_100reps", outfile="AIC_partifionfinder_100runs.txt" )
+aic2 <- summaryaic(dir="output_nomerge_100reps", outfile="AIC_modelselection_nomerge_100runs.txt" )
+
+aic <- rbind(aic1, aic2)                              # combine the merge and no merge results
+aic <- aic[ order(aic$aic), ]	                     # sort by aic score
+
+sink(outfile)                                              # start writing to the output file
+	cat("AIC scores of trees from 100 runs after PartitionFinder and tree estimation:\n")
+	print(aic)
+	cat(paste("delta aic worst - best: ", round(max(aic$aic) - min(aic$aic))))
+sink()
